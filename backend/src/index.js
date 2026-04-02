439a94cfc44a28e560e5359fb90a13b0c0f99e88
Niraj Mahto <itsnirajmahto@gmail.com>
2026-04-02 14:18:03 +0530

    auth

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { requireAuth } = require('../middleware/auth');
+const { requireAuth } = require('./middleware/auth');
 
 const db = require('./db');
 
